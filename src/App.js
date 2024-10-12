685fbb7bff5b1e91984ce97c139504a690327dde
Angelina Chen <angelina.chhen@gmail.com>
2024-10-12 02:32:53 -0400

    Added deadline dates timeline

--- src/App.js
+++ src/App.js
@@ -27,27 +27,75 @@ function Home() {
         <h1 style = {{ color: '#2F2E41', fontSize: '35pt', marginBottom: '-30px', fontWeight: '700', marginTop: '100px'}}> Welcome to </h1>
         <h1 style = {{ color: '#2F2E41', fontSize: '45pt', fontWeight: '700'}}> CivicConnect! <span style = {{color: '#4C63FF', fontSize: '25pt', marginLeft: '20px'}} > One vote, one voice. </span> </h1>
         <p style={{ maxWidth: '700px', color: '#555555', fontSize: '20px', fontWeight: '500', marginTop: '50px', lineHeight: '1.5' }}>
-          Voting is one of the most important ways to express your voice and influence the future of your community. Through the power of voting, let's empower civic engagement and strengthen our democracy!
+          Voting is one of the most important ways to express your voice and influence the future of your community. Through the power of voting, let's empower civic engagement and strengthen our democracy.
         </p>
-        {/* <span style = {{color: '#4C63FF'}} > CivicConnect! </span> */}
       </div>
 
       {/* Adds the image. */}
       <div style={{ flex: '1', textAlign: 'right' }}>
         <img src = {votingImage} alt = "voting_image" style = {{ maxWidth: '50%', marginTop: '-400px', marginLeft: '750px'}} />
       </div>
+
+      {/* Timeline of deadline dates. */}
+      <br></br>
+      <h2 style = {{ marginBottom: '70px' }}> Key upcoming dates and events </h2>
+      <div style = {{ margin: '40px auto' }}>
+        <div style = {{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+          <span> October 26th </span>
+          <span> October 29th </span>
+          <span> November 1st </span>
+          <span> November 5th </span>
+        </div>
+        <div style = {{ margin: '20px 0', backgroundColor: '#4C63FF', height: '4px', position: 'relative' }}>
+          <div style = {{
+            width: '20px',
+            height: '20px',
+            backgroundColor: '#4C63FF',
+            borderRadius: '50%',
+            position: 'absolute',
+            top: '-8px',
+            left: '5%',
+          }}> </div>
+          <div style = {{
+            width: '20px',
+            height: '20px',
+            backgroundColor: '#4C63FF',
+            borderRadius: '50%',
+            position: 'absolute',
+            top: '-8px',
+            left: '35%',
+          }}> </div>
+          <div style = {{
+            width: '20px',
+            height: '20px',
+            backgroundColor: '#4C63FF',
+            borderRadius: '50%',
+            position: 'absolute',
+            top: '-8px',
+            left: '65%',
+          }}> </div>
+          <div style = {{
+            width: '20px',
+            height: '20px',
+            backgroundColor: '#4C63FF',
+            borderRadius: '50%',
+            position: 'absolute',
+            top: '-8px',
+            left: '95%',
+          }}> </div>
+        </div>
+
+        <div style = {{ display: 'flex', justifyContent: 'space-between' }}>
+          <span> Voter Registration Deadline </span>
+          <span> Deadline to Request Ballot </span>
+          <span> Early Voting Deadline </span>
+          <span> Mail and Absentee Deadline </span>
+        </div>
+      </div>
+
     </div>
   );
 }
-
-// function Welcome() {
-//   const { name } = useParams();
-//   return (
-//     <div style = {{ textAlign: 'center', marginTop: '50px' }}>
-//       <h1> Welcome, {name}! </h1>
-//     </div>
-//   );
-// }
 
 function Location() {
   const { location } = useParams();
