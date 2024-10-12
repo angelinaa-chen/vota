f97798e90a5a5e190d4588fcf7e12ecd4a4431e8
amjiao <amyjiaowang@gmail.com>
2024-10-12 16:18:47 -0400

    Implemented get_jurisdictions function - returns all sates

--- src/backend/main.py
+++ src/backend/main.py
@@ -10,23 +10,31 @@ app = Flask(__name__)
 load_dotenv()
 api_key = os.getenv("OPENSTATES_API_KEY")
 app.secret_key = os.urandom(64)
-url="https://v3.openstates.org/jurisdictions?apikey=apikey"
 
 def get_jurisdiction(response):
   if response.status_code == 200:
         data = response.json()
-        # Return names of jurisdictions that have classification = "municipality"
-        return [result['name'] for result in data['results'] if result.get('classification') == "state"]
+        # Return names of jurisdictions that have classification = "state"
+        return [result['name'] for result in data['results']]
   else:
       print(f"Error: {response.status_code} - {response.text}")
       return []  # Return an empty list if there's an error
+  
+#returns response
+def get_id(response):
+  if response.status_code == 200:
+      data = response.json()
+      return [result['id'] for result in data['results']]
+  else:
+    print(f"Error: {response.status_code} - {response.text}")
+    return []  # Return an empty list if there's an error
 
 
 #returns list of all states - will convert so that it returns a jurisdiction id that can then be used to search the API for other information
 @app.route("/jurisdictions")
 def get_jurisdictions():
-  jurisdictions = []
   url="https://v3.openstates.org/jurisdictions?apikey=apikey"
+  jurisdictions = []
   headers = {
     "X-API-KEY": api_key  # Authentication
   }
@@ -42,7 +50,34 @@ def get_jurisdictions():
         jurisdictions.extend(result)  # Aggregate results
 
   return jsonify(jurisdictions)
+  
+#returns list of candidates for a given state 
+  #call jurisdictions, iterate through its json of states, match to get the id
+# @app.route("/candidates/<state>")
+# def get_candidates(state):
+#   candidates = []
+#   url="https://v3.openstates.org/people?apikey=apikey"
+#   headers = {
+#     "X-API-KEY": api_key  # Authentication
+#   }
+#   state_json = get_jurisdictions()
+#   print("State JSON:", state_json)
 
+  
+  
+  # id = ""
+  # state_json = get_jurisdictions()
+  # return print(state_json)
+  # for result in state_json:
+  #   if (result['name'] == state):
+  #     id = result['id']
+  # #id = get_id(juris_id)  # Get id of the jurisdiction
+
+  # if id != "":  # Only extend if result is not empty
+  #   response = requests.get(f"{url}&jurisdiction={id}", headers=headers)
+  #   candidates.extend(response)  # Aggregate results
+
+  # return jsonify(candidates)
 
 # Define a route
 @app.route('/')
