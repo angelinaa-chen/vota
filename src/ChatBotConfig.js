e600d49f81378f5a825b1ee0c58d7d5136caaa4c
Angelina Chen <angelina.chhen@gmail.com>
2024-10-12 18:43:48 -0400

    Added base chatbot files from package

---
+++ src/ChatBotConfig.js
@@ -0,0 +1,16 @@
+import { createChatBotMessage } from 'react-chatbot-kit';
+
+const config = {
+  botName: "LocationBot",
+  initialMessages: [createChatBotMessage(`Heyo!`)],
+  customStyles: {
+    botMessageBox: {
+      backgroundColor: "#4C63FF",
+    },
+    chatButton: {
+      backgroundColor: "#4C63FF",
+    },
+  },
+};
+
+export default config;
