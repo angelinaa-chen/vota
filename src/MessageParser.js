bc2a75aea21afa62377b7927c46ecda050d515a0
Angelina Chen <angelina.chhen@gmail.com>
2024-10-13 03:48:12 -0400

    Chatbot layout messages

--- src/MessageParser.js
+++ src/MessageParser.js
@@ -5,7 +5,13 @@ class MessageParser {
   
     parse(message) {
       const lowerCaseMessage = message.toLowerCase();
-  
+      
+      // Tell me more about the upcoing election.
+      // Tell me about ____ person
+      // Tell me about ___ from Indiana person.
+      // Where are the polls?
+      // When should i mail my ballot. 
+      
       if (lowerCaseMessage.includes("location")) {
         this.actionProvider.handleLocation();
       } else {
