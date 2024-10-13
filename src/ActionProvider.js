36367fa873c5a8327ea266b16e6a4989b135972c
Angelina Chen <angelina.chhen@gmail.com>
2024-10-13 04:52:59 -0400

    Added some functionality for chatbot

--- src/ActionProvider.js
+++ src/ActionProvider.js
@@ -4,19 +4,27 @@ class ActionProvider {
       this.setState = setStateFunc;
     }
   
-    handleLocation = () => {
-      const message = this.createChatBotMessage(
-        "I can help you find a location. Please enter your city and state."
-      );
-      this.addMessageToBotState(message);
-    };
-  
     handleDefault = () => {
       const message = this.createChatBotMessage(
         "I'm not sure how to respond to that. Can you ask something else?"
       );
       this.addMessageToBotState(message);
     };
+
+    handleElectionInfo = () => {
+        const message = this.createChatBotMessage(
+          "The upcoming 2024 U.S. presidential election is set for Tuesday, November 5, 2024. This election will be the 60th quadrennial presidential election and is significant for being the first under the new electoral vote distribution based on the 2020 census. To win, a candidate needs a simple majority of the 538 electoral votes—specifically, 270 votes."
+        );
+        this.addMessageToBotState(message);
+    };
+
+    handleMassachusetts = () => {
+        const message = this.createChatBotMessage(
+          "Massachusetts citizens can fill out the mail ballot application and mail or return it in person to your local election office. You can also submit a mail ballot application online using the Massachusetts Mail Ballot Application System!"
+        );
+        this.addMessageToBotState(message);
+    };
+
   
     addMessageToBotState = (message) => {
       this.setState((prevState) => ({
--- src/MessageParser.js
+++ src/MessageParser.js
@@ -5,20 +5,20 @@ class MessageParser {
   
     parse(message) {
       const lowerCaseMessage = message.toLowerCase();
-      
-      // Tell me more about the upcoing election.
-      // Tell me about ____ person
-      // Tell me about ___ from Indiana person.
-      // Where are the polls?
-      // When should i mail my ballot. 
-      
-      if (lowerCaseMessage.includes("location")) {
-        this.actionProvider.handleLocation();
-      } else {
+
+      if (lowerCaseMessage.includes("massachusetts")) {
+        this.actionProvider.handleMassachusetts();
+      } 
+      else if (lowerCaseMessage.includes("election")) {
+        this.actionProvider.handleElectionInfo();
+      }
+      else {
         this.actionProvider.handleDefault();
       }
     }
   }
   
   export default MessageParser;
+
+
   
